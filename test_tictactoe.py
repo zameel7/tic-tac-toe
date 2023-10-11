65ef11c597a645d6d8cc4d5e84797db12997f28b
Rahul Saini <137695929+ProgramR4732@users.noreply.github.com>
2023-10-11 20:31:16 +0530

    Optimized assert statements.
    -> the boolean references are not necessary.

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -3,15 +3,15 @@ from tictactoe import choose_first, space_check, full_board_check, marker_positi
 # unit tests for the code implemented
 
 def test_full_board_check():
-    assert full_board_check([" "]*10) == False
+    assert not full_board_check([" "]*10)
 
 def test_choose_first():
     assert choose_first("X") == "Player 1"
     assert choose_first("O") == "Player 2"
 
 def test_space_check():
-    assert space_check([" "]*10, 3) == True
+    assert space_check([" "] * 10, 3)
 
 def test_marker_position():
-    assert marker_position(2) == True
-    assert marker_position(12) ==False
+    assert marker_position(2)
+    assert not marker_position(12)
